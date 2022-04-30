17c556eb502504e86a0b3ddff44ba438a9d1b643
Yeabsira <yeabsiratilahun78@gmail.com>
2022-05-01 00:40:08 +0300

    cart added

--- index.js
+++ index.js
@@ -68,3 +68,22 @@ function fixedNav() {
 }
 
 window.addEventListener('scroll', fixedNav);
+
+//for cart
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+let a = 1;
+plus.addEventListener("click", ()=>{
+  a++;
+  a = (a < 10) ? "0" + a : a;
+  num.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+  if(a > 1){
+    a--;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+  }
+});
