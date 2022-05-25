32115bd799d93f96b81de655693bfac986e40071
Yeabsira <yeabsiratilahun78@gmail.com>
2022-05-25 06:12:09 +0300

    SIgnin and SIgnup works

--- SignIn/Sign-in.php
+++ SignIn/Sign-in.php
@@ -1,24 +1,39 @@
 <?php
-session_start();
-include 'Connect.php';
+require("../connect.php");
+$con=getConnect(); 
 
+if(isset($_POST['submit']))
+{
 $username=$_POST['username'];
 $password=$_POST['password'];
 $query="select * from Customers where email='$username' and PassWrd='$password'";
-$result=mysqli_query($conn,$query);
-$count=mysqli_num_rows($result);
+$result=sqlsrv_query($con, $query);
+// $count=mysqli_num_rows($result);
 
 // if($conn== false)
 //     die(print_r(sqlsrv_errors(),true));
 // else
 //     echo'Connection Success';
-    if($count>0){
-        echo'Welcome!';
-        header("Location: HomePage.html");
+if ($result == false){
+    echo'The error is around the database';
     }
     else{
-        echo'You enter a wrong username or password';
+        while ($row = sqlsrv_fetch_array($result)) {
+         if($row['Email']==$username && $row['PassWrd']==$password)
+        {
+            
+            session_start();
+            $_SESSION['username']=$username;
+            $_SESSION['password']=$password;
+            header("location:../index.php");
+        }
     }
-
+          
+    echo 'You entered the wrong username or password';
+        //    header("location: Sign-in.html");
+    }
+    
+    
+    }
 
 ?>
--- SignUp/Create Account.php
+++ SignUp/Create Account.php
@@ -1,26 +1,77 @@
-<?php
-include 'Connect.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Account</title>
+    <link rel="stylesheet" href="../SignUp/Create Account.css">
+    <script src="Create Account.js" defer></script>
+</head>
 
-if($conn== false)
-    die(print_r(sqlsrv_errors(),true));
-else
-    if(!empty($_POST['save'])){
+<body>
+
+    <section class="container">
+        <form action="Create Account2.php" method="post" enctype='multipart/form-data'>
+
+        <article class="advert"><img src="../Images/Welcome to Senq marketplace.png" alt="Advert" class="advert"></article>
+
+            <div class="signup">
+            <h2 class="title">Create Account</h2>
+            
+            <div><p><br><br><br></p></div>  <!-- To create space between the h2 and the img-->
+            
+         <div class="pic">
+             <img src="../Images/Profile.jpg" id="photo">
+             <input type="file" name="pp" id="file" id="pp">
+             <label for="file" id="uploadBtn"><img src="../Images/addImg.png"></label>
+
+             <!-- <input type="file" name="imgFile" id="imgFile" accept="images/*" required />
+            <label for="imgFile"></label> -->
+         </div>
+
+            <article class="fname"> <!-- FirstName -->
+                <input type="text" name="fname" placeholder="First Name" required>
+                <article class="underline"></article>
+            </article>
+
+            <article class="lname"> <!-- LastName -->
+                <input type="text" name="lname" placeholder="Last Name" required>
+                <article class="underline"></article>
+            </article>
+
+            <article class="email"> <!-- Email -->
+                <input type="text" name="email" placeholder="Email" required>
+                <article class="underline"></article>
+            </article>
+
+            <article class="email"> <!-- password -->
+                <input type="password" name="pass" placeholder="password" required>
+                <article class="underline"></article>
+            </article>
+
+            <article class="phone"> <!-- phonenumber -->
+                <input type="text" name="phone" placeholder="Phone Number" required>
+                <article class="underline"></article>
+            </article>
+
+            <article class="create">
+                <input type="submit" name="save" value="Create Account">
+
+            </article>
+
+            <article class="link">
+                <p>Have an account? <a href="../SignIn/Sign-in.html">Sign-In</a></p><br>
+
+            </article>
+
+            <article class="trade">
+                <p>Senq Marketplace &trade;</p>
+            </article>
+        </div>
     
-        $fname=$_POST['fname'];
-        $lname=$_POST['lname'];
-        $email=$_POST['email'];
-        $phone=$_POST['phone'];
-        $query="insert into Customers (Fname,Lname,Email,phoneNo) values('$fname','$lname','$email','$phone')";
-        $result=sqlsrv_query($conn,$query);
-
-        if($result){
-            echo'Welcome to Senq Marketplace';
-        }
-
-        else{
-            echo'Please fill out all the fields correctly';
-        }
-}
-
-
-?>
+        </form>
+    </section>
+  
+</body>
+</html>
